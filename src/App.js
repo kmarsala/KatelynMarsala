ce4f2dde4c4a006b9dd2db87f816a82590d1153d
kmarsala <kmarsala5@hotmail.com>
2020-10-21 22:05:36 -0400

    upgrade dependencies and playing with antd

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import { Layout } from 'antd';
 import MyHeader from './components/MyHeader';
 import MainContent from './components/MainContent';
+import SideNav from './components/SideNav';
+import 'antd/dist/antd.css';
+
+
 import './App.less';
 
 function App() {
   return (
     <div className="App">
       <Layout>
+        <SideNav />
         <MyHeader />
         <MainContent />
       </Layout>
