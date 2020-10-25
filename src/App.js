61d396f8ade0b24ec9132bfb57e0f50025e45ece
kmarsala <kmarsala5@hotmail.com>
2020-10-24 20:38:38 -0400

    background and nav bar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { Layout } from 'antd';
+import { Layout, } from 'antd';
 import MyHeader from './components/Layout/MyHeader';
-import background from './images/background.jpg';
-import MainContent from './components/Layout/MainContent';
-import SideNav from './components/Layout/SideNav';
 import 'antd/dist/antd.css';
 
 
 import './App.less';
 
+
+
+
 function App() {
   return (
     <Layout className="main-layout">
       <MyHeader />
+
       {/* <Layout style={{ minHeight: '100vh' }}> */}
 
       {/* <SideNav /> */}
--- src/components/Layout/MyHeader.js
+++ src/components/Layout/MyHeader.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Layout } from 'antd';
 import { LinkedinOutlined } from '@ant-design/icons';
+import MyMenu from './MyMenu'
 
 import './MyHeader.less';
 
@@ -9,6 +10,7 @@ const { Header } = Layout;
 const MyHeader = () => {
   return <Header className="header">
     <h1>Katelyn Marsala</h1>
+    <MyMenu />
     <LinkedinOutlined className="header-icon" />
   </Header>;
 };
